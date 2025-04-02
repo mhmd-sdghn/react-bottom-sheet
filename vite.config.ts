606fe462f9b501258c39e9a3705f8df13eb69e34
Mo <mo.sdghn@gmail.com>
2025-04-02 14:14:29 +0330

    [refactor] added node types

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
+import path from "path";
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": "./src",
-      "@lib": "./lib",
-      "@examples": "./examples",
+      "@": path.resolve(__dirname, "./src"),
+      "@lib": path.resolve(__dirname, "./lib"),
+      "@appTypes": path.resolve(__dirname, "./types"),
+      "@examples": path.resolve(__dirname, "./examples"),
     },
   },
   plugins: [
