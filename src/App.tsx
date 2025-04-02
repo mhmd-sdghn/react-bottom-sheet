5aa2d8844268cb8c497c691ad95b45bb20602be4
Mo <mo.sdghn@gmail.com>
2025-04-02 13:53:50 +0330

    [refactor] used Sheet component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Sheet, SnapPoints } from "@lib/index";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isOpen, setIsOpen] = useState(false);
+  const [snapPoints, setSnapPoints] = useState<SnapPoints>([]);
+  const [activeSnapPointIndex, setActiveSnapPointIndex] = useState(0);
+
+  const onSnap = (index: number) => {
+    setActiveSnapPointIndex(index);
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  const onSnapPointsUpdate = (snapPoints: SnapPoints) => {
+    setSnapPoints(snapPoints);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Sheet
+        isOpen={isOpen}
+        snapPoints={snapPoints}
+        activeSnapPointIndex={activeSnapPointIndex}
+        onSnap={onSnap}
+        onClose={onClose}
+        onSnapPointsUpdate={onSnapPointsUpdate}
+      ></Sheet>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
