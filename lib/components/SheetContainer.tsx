321892caf9243660cf67f1449abf342e6158a268
Mo <mo.sdghn@gmail.com>
2025-04-02 16:16:12 +0330

    [refactor] smoother animation function

--- lib/components/SheetContainer.tsx
+++ lib/components/SheetContainer.tsx
@@ -1,46 +1,47 @@
-import { motion, AnimatePresence } from "motion/react";
+import { motion, AnimatePresence, MotionConfig } from "motion/react";
 import { Children, FC, ReactNode, useState } from "react";
 import { useSheetContext } from "../context.tsx";
-import { findHeaderComponent, isSSR } from "../utils.ts";
+import { findHeaderComponent } from "../utils.ts";
 import SheetDynamicHeightContent from "./SheetDynamicHeightContent.tsx";
 
 const SheetContainer: FC<{ children: ReactNode }> = ({ children }) => {
   const state = useSheetContext();
-  const [y] = useState(-100);
+  const [y] = useState(-200);
   const HeaderComponent = findHeaderComponent(children);
 
   const onHeightChange = () => {};
 
   return (
-    <AnimatePresence>
-      {state.isOpen && (
-        <motion.div
-          initial={!isSSR()}
-          animate={{ y }}
-          exit={{ y: 0 }}
-          style={{
-            position: "fixed",
-            bottom: "-100%",
-            height: "100dvh",
-            left: 0,
-            right: 0,
-            background: "gray",
-          }}
-        >
-          {HeaderComponent ? (
-            <>
-              <SheetDynamicHeightContent
-                onHeightChange={onHeightChange}
-                {...HeaderComponent.props}
-              />
-              {Children.toArray(children).slice(1)}
-            </>
-          ) : (
-            children
-          )}
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <MotionConfig transition={{ type: "spring", restSpeed: 0.1, bounce: 0.3 }}>
+      <AnimatePresence>
+        {state.isOpen && (
+          <motion.div
+            animate={{ y }}
+            exit={{ y: 0 }}
+            style={{
+              position: "fixed",
+              bottom: "-100%",
+              height: "100dvh",
+              left: 0,
+              right: 0,
+              background: "gray",
+            }}
+          >
+            {HeaderComponent ? (
+              <>
+                <SheetDynamicHeightContent
+                  onHeightChange={onHeightChange}
+                  {...HeaderComponent.props}
+                />
+                {Children.toArray(children).slice(1)}
+              </>
+            ) : (
+              children
+            )}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </MotionConfig>
   );
 };
 
