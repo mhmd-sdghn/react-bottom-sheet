d0ced455d503b804347cd101f906a26d23cf937d
Mo <mo.sdghn@gmail.com>
2025-04-03 16:42:26 +0330

    [feat] added new constants, required in drag calculations

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,13 @@
 export const HeaderComponentId = "REACT_BOTTOM_SHEET_HEADER_COMPONENT";
+
+export const DragOffsetThreshold = 80;
+
+export const DragVelocityThreshold = 500;
+
+export const DragCloseThreshold = 0.6;
+
+export const TweenAnimationConfig = {
+  type: "tween",
+  ease: "easeOut",
+  duration: 0.2,
+};
