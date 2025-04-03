13bd333fc5b5dd7f5732bb53fe54f37fab846e2b
Mo <mo.sdghn@gmail.com>
2025-04-03 16:42:57 +0330

    [chore] update the working examplee

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { Sheet, SnapPoints } from "@lib/index";
 import { useState } from "react";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(true);
-  const [snapPoints, setSnapPoints] = useState<SnapPoints>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [snapPoints, setSnapPoints] = useState<SnapPoints>([400, 650, 1]);
   const [activeSnapPointIndex, setActiveSnapPointIndex] = useState(0);
 
   const onSnap = (index: number) => {
+    console.log("onSnap", index);
     setActiveSnapPointIndex(index);
   };
 
   const onClose = () => {
+    setActiveSnapPointIndex(0);
     setIsOpen(false);
   };
 
@@ -20,7 +22,17 @@ function App() {
 
   return (
     <>
-      <button onClick={() => setIsOpen((value) => !value)}>
+      <div
+        style={{
+          height: 10,
+          width: "100%",
+          background: "yellow",
+          position: "fixed",
+          bottom: 650,
+          zIndex: 9999,
+        }}
+      ></div>
+      <button onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? "hide" : "show"}
       </button>
       <Sheet
@@ -33,7 +45,15 @@ function App() {
       >
         <Sheet.Container>
           <Sheet.Header>
-            <h1>header</h1>
+            <div
+              style={{
+                background: "red",
+                height: activeSnapPointIndex === 1 ? 400 : 200,
+                transition: "all ease 0.5s",
+              }}
+            >
+              <h1>header</h1>
+            </div>
           </Sheet.Header>
           <div>
             <h2>Content</h2>
